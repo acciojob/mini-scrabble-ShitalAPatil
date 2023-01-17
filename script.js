89db116cb47b66ee1f0d46ee239e8261c213061c
ShitalAPatil <shitalpatilb29@gmail.com>
2023-01-17 15:58:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 //your code here
+let evaluatedText = document.getElementById("evaluatedText");
+let letterCount = document.getElementById("letterCount");
+evaluatedText.addEventListener("input",calculateLength)
 
+function calculateLength(){
+	let wordLength = word.length;
+	letterCount.innerHTML=wordLength;
+}
