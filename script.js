1cb5225e62366c18959e09082fe665a292fc81b1
ShitalAPatil <shitalpatilb29@gmail.com>
2023-01-17 16:05:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 //your code here
-let evaluatedText = document.getElementById("evaluatedText");
-let letterCount = document.getElementById("letterCount");
-evaluatedText.addEventListener("input",calculateLength)
+// let evaluatedText = document.getElementById("evaluatedText");
+// let letterCount = document.getElementById("letterCount");
+// evaluatedText.addEventListener("input",calculateLength)
 
-function calculateLength(){
-	let wordLength = word.length;
-	letterCount.innerHTML=wordLength;
+// function calculateLength(){
+// 	let wordLength = word.length;
+// 	letterCount.innerHTML=wordLength;
+// }
+
+function countChars(obj){
+    document.getElementById("letterCount").innerHTML = obj.value.length;
 }
